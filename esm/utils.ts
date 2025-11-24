d22efe5226f03d4004bc2402b62b454b8f046300
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2025-11-24 12:49:48 +0100

    feat: added areEqualTypes

--- esm/utils.ts
+++ esm/utils.ts
@@ -9,58 +9,58 @@ const universalPathSep = /[/\\]/;
 
 /** Throws an `AssertionError` with the provided error message. */
 export function fail(message: string) {
-	assert.fail(message);
+  assert.fail(message);
 }
 
 export function deepStrictEqual<A>(actual: A, expected: A, message?: string) {
-	assert.deepStrictEqual(actual, expected, message);
+  assert.deepStrictEqual(actual, expected, message);
 }
 
 export function strictEqual<A>(actual: A, expected: A, message?: string) {
-	assert.strictEqual(actual, expected, message);
+  assert.strictEqual(actual, expected, message);
 }
 
 /** Asserts that `actual` is equal to `expected` using the `Equal.equals` trait. */
 export function assertEquals<A>(actual: A, expected: A, message?: string) {
-	if (!Utils.structuralRegion(() => Equal.equals(actual, expected))) {
-		deepStrictEqual(actual, expected, message); // show diff
-		fail(message ?? 'Expected values to be Equal.equals');
-	}
+  if (!Utils.structuralRegion(() => Equal.equals(actual, expected))) {
+    deepStrictEqual(actual, expected, message); // show diff
+    fail(message ?? 'Expected values to be Equal.equals');
+  }
 }
 
 /** Asserts that `actual` is not equal to `expected` using the `Equal.equals` trait. */
 export function assertNotEquals<A, B>(actual: A, expected: B, message?: string) {
-	assertFalse(
-		Utils.structuralRegion(() => Equal.equals(actual, expected)),
-		message
-	);
+  assertFalse(
+    Utils.structuralRegion(() => Equal.equals(actual, expected)),
+    message,
+  );
 }
 
 export function assertTrue(self: boolean, message?: string): asserts self is true {
-	strictEqual(self, true, message);
+  strictEqual(self, true, message);
 }
 
 export function assertFalse(self: boolean, message?: string): asserts self is false {
-	strictEqual(self, false, message);
+  strictEqual(self, false, message);
 }
 
 // ----------------------------
 // Option
 // ----------------------------
 export function assertNone<A>(
-	option: Option.Option<A>,
-	message?: string
+  option: Option.Option<A>,
+  message?: string,
 ): asserts option is Option.None<never> {
-	assertEquals(option, Option.none(), message);
+  assertEquals(option, Option.none(), message);
 }
 
 export function assertSome<A>(
-	option: Option.Option<A>,
-	expected?: A,
-	message?: string
+  option: Option.Option<A>,
+  expected?: A,
+  message?: string,
 ): asserts option is Option.Some<A> {
-	if (expected === undefined) assertTrue(Option.isSome(option), message);
-	else assertEquals(option, Option.some(expected), message);
+  if (expected === undefined) assertTrue(Option.isSome(option), message);
+  else assertEquals(option, Option.some(expected), message);
 }
 
 // ----------------------------
@@ -68,53 +68,51 @@ export function assertSome<A>(
 // ----------------------------
 
 export function assertLeft<R, L>(
-	either: Either.Either<R, L>,
-	expected?: L,
-	message?: string
+  either: Either.Either<R, L>,
+  expected?: L,
+  message?: string,
 ): asserts either is Either.Left<L, never> {
-	if (expected === undefined) assertTrue(Either.isLeft(either), message);
-	else assertEquals(either, Either.left(expected), message);
+  if (expected === undefined) assertTrue(Either.isLeft(either), message);
+  else assertEquals(either, Either.left(expected), message);
 }
 
 export function assertLeftMessage<R, L extends { readonly message: string }>(
-	either: Either.Either<R, L>,
-	expected: string,
-	message?: string
+  either: Either.Either<R, L>,
+  expected: string,
+  message?: string,
 ): asserts either is Either.Left<L, never> {
-	assertLeft(either, undefined, message);
-	strictEqual(either.left.message, expected, message);
+  assertLeft(either, undefined, message);
+  strictEqual(either.left.message, expected, message);
 }
 
 export function assertRight<R, L>(
-	either: Either.Either<R, L>,
-	expected?: R,
-	message?: string
+  either: Either.Either<R, L>,
+  expected?: R,
+  message?: string,
 ): asserts either is Either.Right<never, R> {
-	if (expected === undefined) assertTrue(Either.isRight(either), message);
-	else assertEquals(either, Either.right(expected), message);
+  if (expected === undefined) assertTrue(Either.isRight(either), message);
+  else assertEquals(either, Either.right(expected), message);
 }
 
 // ----------------------------
 // Throwing
 // ----------------------------
 export function throws(thunk: () => void, error?: Error | ((u: unknown) => undefined)) {
-	/* eslint-disable-next-line functional/no-let*/
-	let throwError;
-	/* eslint-disable-next-line functional/no-try-statements*/
-	try {
-		thunk();
-		throwError = true;
-	} catch (e) {
-		throwError = false;
-		if (error !== undefined) {
-			if (Predicate.isFunction(error)) {
-				error(e);
-			} else {
-				deepStrictEqual(e, error);
-			}
-		}
-	}
-	if (throwError) fail('Expected to throw an error');
+  let throwError;
+  try {
+    thunk();
+    throwError = true;
+  } catch (e) {
+    throwError = false;
+    if (error !== undefined) {
+      if (Predicate.isFunction(error)) {
+        error(e);
+      } else {
+        deepStrictEqual(e, error);
+      }
+    }
+  }
+  if (throwError) fail('Expected to throw an error');
 }
 
 /**
@@ -123,7 +121,7 @@ export function throws(thunk: () => void, error?: Error | ((u: unknown) => undef
  * @since 0.21.0
  */
 export function doesNotThrow(thunk: () => void, message?: string) {
-	assert.doesNotThrow(thunk, message);
+  assert.doesNotThrow(thunk, message);
 }
 
 /**
@@ -133,27 +131,27 @@ export function doesNotThrow(thunk: () => void, message?: string) {
  * @category Utils
  */
 export const moduleTagFromTestFilePath = (filePath: string): Option.Option<string> =>
-	Option.gen(function* () {
-		const pathParts = filePath.split(universalPathSep);
-		const pathPartsLength = pathParts.length;
-		const packageName = yield* pipe(
-			pathParts,
-			Array.get(pathPartsLength - 3),
-			Option.filter(String.isNonEmpty)
-		);
-		const testFileName = yield* pipe(pathParts, Array.get(pathPartsLength - 1));
-		yield* pipe(
-			testFileName,
-			String.takeRight(8),
-			Option.liftPredicate((s) => s === '.test.ts')
-		);
-		const moduleName = yield* pipe(
-			testFileName,
-			String.takeLeft(testFileName.length - 8),
-			Option.liftPredicate(String.isNonEmpty)
-		);
-		return '@parischap/' + packageName + '/' + moduleName + '/';
-	});
+  Option.gen(function* () {
+    const pathParts = filePath.split(universalPathSep);
+    const pathPartsLength = pathParts.length;
+    const packageName = yield* pipe(
+      pathParts,
+      Array.get(pathPartsLength - 3),
+      Option.filter(String.isNonEmpty),
+    );
+    const testFileName = yield* pipe(pathParts, Array.get(pathPartsLength - 1));
+    yield* pipe(
+      testFileName,
+      String.takeRight(8),
+      Option.liftPredicate((s) => s === '.test.ts'),
+    );
+    const moduleName = yield* pipe(
+      testFileName,
+      String.takeLeft(testFileName.length - 8),
+      Option.liftPredicate(String.isNonEmpty),
+    );
+    return '@parischap/' + packageName + '/' + moduleName + '/';
+  });
 
 /**
  * Function that returns a `true` type if the two type parameters are equal. Returns a `false` type
@@ -163,9 +161,16 @@ export const moduleTagFromTestFilePath = (filePath: string): Option.Option<strin
  */
 
 export function areEqualTypes<A, B>(): readonly [A] extends readonly [B] ?
-	readonly [B] extends readonly [A] ?
-		true
-	:	false
-:	false {
-	return undefined as never;
+  readonly [B] extends readonly [A] ?
+    true
+  : false
+: false {
+  return undefined as never;
 }
+
+/**
+ * Function that asserts a `true` type
+ *
+ * @category Utils
+ */
+export function assertTrueType<T extends true>(_t: T): void {}
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -3,142 +3,145 @@ import { Array, Either, Equal, Option } from 'effect';
 import { describe, expect, it } from 'vitest';
 
 describe('TEUtils', () => {
-	describe('assertEquals', () => {
-		it('Primitive values', () => {
-			expect(() => TEUtils.assertEquals(1, 1)).not.toThrow();
-		});
+  describe('assertEquals', () => {
+    it('Primitive values', () => {
+      expect(() => TEUtils.assertEquals(1, 1)).not.toThrow();
+    });
 
-		it('Arrays of options matching', () => {
-			expect(() =>
-				TEUtils.assertEquals(
-					Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
-					Array.make(Option.some(1), Array.make(Option.none(), Option.some(3)))
-				)
-			).not.toThrow();
-		});
+    it('Arrays of options matching', () => {
+      expect(() =>
+        TEUtils.assertEquals(
+          Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
+          Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
+        ),
+      ).not.toThrow();
+    });
 
-		it('Arrays of options not matching', () => {
-			expect(() =>
-				TEUtils.assertEquals(
-					Array.make(Option.some(1), Option.some(2)),
-					Array.make(Option.some(1), Option.none())
-				)
-			).toThrow();
-		});
-	});
+    it('Arrays of options not matching', () => {
+      expect(() =>
+        TEUtils.assertEquals(
+          Array.make(Option.some(1), Option.some(2)),
+          Array.make(Option.some(1), Option.none()),
+        ),
+      ).toThrow();
+    });
+  });
 
-	describe('assertNotEquals', () => {
-		it('Primitive values', () => {
-			expect(() => TEUtils.assertNotEquals(1, 2)).not.toThrow();
-		});
+  describe('assertNotEquals', () => {
+    it('Primitive values', () => {
+      expect(() => TEUtils.assertNotEquals(1, 2)).not.toThrow();
+    });
 
-		it('Arrays of options matching', () => {
-			expect(() =>
-				TEUtils.assertNotEquals(
-					Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
-					Array.make(Option.some(1), Array.make(Option.none(), Option.some(3)))
-				)
-			).toThrow();
-		});
+    it('Arrays of options matching', () => {
+      expect(() =>
+        TEUtils.assertNotEquals(
+          Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
+          Array.make(Option.some(1), Array.make(Option.none(), Option.some(3))),
+        ),
+      ).toThrow();
+    });
 
-		it('Arrays of options not matching', () => {
-			expect(() =>
-				TEUtils.assertNotEquals(
-					Array.make(Option.some(1), Option.some(2)),
-					Array.make(Option.some(1), Option.none())
-				)
-			).not.toThrow();
-		});
-	});
+    it('Arrays of options not matching', () => {
+      expect(() =>
+        TEUtils.assertNotEquals(
+          Array.make(Option.some(1), Option.some(2)),
+          Array.make(Option.some(1), Option.none()),
+        ),
+      ).not.toThrow();
+    });
+  });
 
-	describe('assertTrue', () => {
-		it('should assert that a value is true', () => {
-			expect(() => TEUtils.assertTrue(true)).not.toThrow();
-			expect(() => TEUtils.assertTrue(false)).toThrow();
-		});
-	});
+  describe('assertTrue', () => {
+    it('should assert that a value is true', () => {
+      expect(() => TEUtils.assertTrue(true)).not.toThrow();
+      expect(() => TEUtils.assertTrue(false)).toThrow();
+    });
+  });
 
-	describe('assertFalse', () => {
-		it('should assert that a value is false', () => {
-			expect(() => TEUtils.assertFalse(false)).not.toThrow();
-			expect(() => TEUtils.assertFalse(true)).toThrow();
-		});
-	});
+  describe('assertFalse', () => {
+    it('should assert that a value is false', () => {
+      expect(() => TEUtils.assertFalse(false)).not.toThrow();
+      expect(() => TEUtils.assertFalse(true)).toThrow();
+    });
+  });
 
-	describe('assertNone', () => {
-		it('should assert that an Option is none', () => {
-			expect(() => TEUtils.assertNone(Option.none())).not.toThrow();
-			expect(() => TEUtils.assertNone(Option.some(1))).toThrow();
-		});
-	});
+  describe('assertNone', () => {
+    it('should assert that an Option is none', () => {
+      expect(() => TEUtils.assertNone(Option.none())).not.toThrow();
+      expect(() => TEUtils.assertNone(Option.some(1))).toThrow();
+    });
+  });
 
-	describe('assertSome', () => {
-		it('should assert that an Option is some with the expected value', () => {
-			expect(() => TEUtils.assertSome(Option.some(1), 1)).not.toThrow();
-			expect(() => TEUtils.assertSome(Option.some(1))).not.toThrow();
-			expect(() => TEUtils.assertSome(Option.some(1), 2)).toThrow();
-			expect(() => TEUtils.assertSome(Option.none(), 2)).toThrow();
-		});
-	});
+  describe('assertSome', () => {
+    it('should assert that an Option is some with the expected value', () => {
+      expect(() => TEUtils.assertSome(Option.some(1), 1)).not.toThrow();
+      expect(() => TEUtils.assertSome(Option.some(1))).not.toThrow();
+      expect(() => TEUtils.assertSome(Option.some(1), 2)).toThrow();
+      expect(() => TEUtils.assertSome(Option.none(), 2)).toThrow();
+    });
+  });
 
-	describe('assertLeft', () => {
-		it('should assert that an Either is left with the expected value', () => {
-			expect(() => TEUtils.assertLeft(Either.left('foo'), 'foo')).not.toThrow();
-			expect(() => TEUtils.assertLeft(Either.left('foo'))).not.toThrow();
-			expect(() => TEUtils.assertLeft(Either.left('foo'), 'bar')).toThrow();
-			expect(() => TEUtils.assertLeft(Either.right('foo'), 'foo')).toThrow();
-		});
-	});
+  describe('assertLeft', () => {
+    it('should assert that an Either is left with the expected value', () => {
+      expect(() => TEUtils.assertLeft(Either.left('foo'), 'foo')).not.toThrow();
+      expect(() => TEUtils.assertLeft(Either.left('foo'))).not.toThrow();
+      expect(() => TEUtils.assertLeft(Either.left('foo'), 'bar')).toThrow();
+      expect(() => TEUtils.assertLeft(Either.right('foo'), 'foo')).toThrow();
+    });
+  });
 
-	describe('assertLeftMessage', () => {
-		it('should assert that an Either is left with the expected message', () => {
-			expect(() => TEUtils.assertLeftMessage(Either.left(new Error('foo')), 'foo')).not.toThrow();
-			expect(() => TEUtils.assertLeftMessage(Either.left(new Error('foo')), 'bar')).toThrow();
-			expect(() => TEUtils.assertLeftMessage(Either.right(new Error('foo')), 'foo')).toThrow();
-		});
-	});
+  describe('assertLeftMessage', () => {
+    it('should assert that an Either is left with the expected message', () => {
+      expect(() => TEUtils.assertLeftMessage(Either.left(new Error('foo')), 'foo')).not.toThrow();
+      expect(() => TEUtils.assertLeftMessage(Either.left(new Error('foo')), 'bar')).toThrow();
+      expect(() => TEUtils.assertLeftMessage(Either.right(new Error('foo')), 'foo')).toThrow();
+    });
+  });
 
-	describe('assertRight', () => {
-		it('should assert that an Either is right with the expected value', () => {
-			expect(() => TEUtils.assertRight(Either.right(42), 42)).not.toThrow();
-			expect(() => TEUtils.assertRight(Either.right(42))).not.toThrow();
-			expect(() => TEUtils.assertRight(Either.right(42), 43)).toThrow();
-			expect(() => TEUtils.assertRight(Either.left(42), 42)).toThrow();
-		});
-	});
+  describe('assertRight', () => {
+    it('should assert that an Either is right with the expected value', () => {
+      expect(() => TEUtils.assertRight(Either.right(42), 42)).not.toThrow();
+      expect(() => TEUtils.assertRight(Either.right(42))).not.toThrow();
+      expect(() => TEUtils.assertRight(Either.right(42), 43)).toThrow();
+      expect(() => TEUtils.assertRight(Either.left(42), 42)).toThrow();
+    });
+  });
 
-	describe('throws', () => {
-		it('should assert that an error is thrown', () => {
-			expect(() => TEUtils.throws(() => BigInt(Infinity))).not.toThrow();
-			expect(() => TEUtils.throws(() => BigInt(5))).toThrow();
-		});
-	});
+  describe('throws', () => {
+    it('should assert that an error is thrown', () => {
+      expect(() => TEUtils.throws(() => BigInt(Infinity))).not.toThrow();
+      expect(() => TEUtils.throws(() => BigInt(5))).toThrow();
+    });
+  });
 
-	describe('doesNotThrow', () => {
-		it('should assert that an error is not thrown', () => {
-			expect(() => TEUtils.doesNotThrow(() => BigInt(5))).not.toThrow();
-			expect(() => TEUtils.doesNotThrow(() => BigInt(Infinity))).toThrow();
-		});
-	});
+  describe('doesNotThrow', () => {
+    it('should assert that an error is not thrown', () => {
+      expect(() => TEUtils.doesNotThrow(() => BigInt(5))).not.toThrow();
+      expect(() => TEUtils.doesNotThrow(() => BigInt(Infinity))).toThrow();
+    });
+  });
 
-	describe('moduleTagFromTestFilePath', () => {
-		it('should return the module tag for a valid test file path', () => {
-			expect(
-				Equal.equals(
-					TEUtils.moduleTagFromTestFilePath('C:/project/packages/module/tests/example.test.ts'),
-					Option.some('@parischap/module/example/')
-				)
-			).toBe(true);
-		});
+  describe('moduleTagFromTestFilePath', () => {
+    it('should return the module tag for a valid test file path', () => {
+      expect(
+        Equal.equals(
+          TEUtils.moduleTagFromTestFilePath('C:/project/packages/module/tests/example.test.ts'),
+          Option.some('@parischap/module/example/'),
+        ),
+      ).toBe(true);
+    });
 
-		it('should return none for an invalid test file path', () => {
-			expect(Option.isNone(TEUtils.moduleTagFromTestFilePath('C:/project/tests/example.js'))).toBe(
-				true
-			);
-		});
-	});
+    it('should return none for an invalid test file path', () => {
+      expect(Option.isNone(TEUtils.moduleTagFromTestFilePath('C:/project/tests/example.js'))).toBe(
+        true,
+      );
+    });
+  });
 
-	TEUtils.areEqualTypes<number, number>() satisfies true;
-	TEUtils.areEqualTypes<number, 3>() satisfies false;
-	TEUtils.areEqualTypes<3, number>() satisfies false;
+  /* eslint-disable-next-line functional/no-expression-statements */
+  TEUtils.areEqualTypes<number, number>() satisfies true;
+  /* eslint-disable-next-line functional/no-expression-statements */
+  TEUtils.areEqualTypes<number, 3>() satisfies false;
+  /* eslint-disable-next-line functional/no-expression-statements */
+  TEUtils.areEqualTypes<3, number>() satisfies false;
 });
